d25264caf096991881a383be74c8790b856457b3
AlexVotry <alexvotry@gmail.com>
2019-04-30 08:20:01 -0700

    made active links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/alexLogo.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [homeClass, setHomeClass] = useState('active');
+  const [listClass, setListClass] = useState('');
+
+  const changeClass = (page) => {
+    if (page === 'home') {
+      setHomeClass('active');
+      setListClass('')
+    } else {
+      setListClass('active');
+      setHomeClass('');
+    }
+  }
+
   return (
     <div className="App-header">
       <ul className="pageLinks">
-        <li><Link to="/">Home</Link></li>
-        <li> <Link to="/list">List of Projects</Link></li>
+        <li><Link to="/" onClick={() => changeClass('home')} className={homeClass}>Home</Link></li>
+        <li> <Link to="/list" onClick={() => changeClass('list')} className={listClass}>List of Projects</Link></li>
       </ul>
       <img className="logo" src={logo} alt="alex-logo" />
     </div>
