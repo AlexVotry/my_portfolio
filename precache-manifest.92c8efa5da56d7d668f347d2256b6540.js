9715c8ac26893b0530372a605d4cada5a9a0a15e
AlexVotry <alexvotry@gmail.com>
2019-04-30 19:36:25 -0700

    Updates

---
+++ precache-manifest.92c8efa5da56d7d668f347d2256b6540.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e632c011f02c2d1cf592c402613667f",
+    "url": "/my_portfolio/static/media/playButton.0e632c01.svg"
+  },
+  {
+    "revision": "6d33e91ca8655f404c17b0467c5aef11",
+    "url": "/my_portfolio/static/media/github.6d33e91c.svg"
+  },
+  {
+    "revision": "5030991f8701b31f5e246b16fea577e0",
+    "url": "/my_portfolio/static/media/alexLogo.5030991f.png"
+  },
+  {
+    "revision": "f4e0835a9f15902d0882",
+    "url": "/my_portfolio/static/js/runtime~main.bc1dbe82.js"
+  },
+  {
+    "revision": "5efc4cbd17e11a4dd7e5",
+    "url": "/my_portfolio/static/js/main.6ba60f01.chunk.js"
+  },
+  {
+    "revision": "1beabfea3d78d6b18093",
+    "url": "/my_portfolio/static/js/2.88e1d0ca.chunk.js"
+  },
+  {
+    "revision": "5efc4cbd17e11a4dd7e5",
+    "url": "/my_portfolio/static/css/main.1c4da775.chunk.css"
+  },
+  {
+    "revision": "08ab710da874fe7297f24dd1431d6ba0",
+    "url": "/my_portfolio/index.html"
+  }
+];
