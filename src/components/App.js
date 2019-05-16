78f656382271975791eda4aed6cf15632ae52b0e
Alex Votry <alexvotry@gmail.com>
2019-05-15 20:33:19 -0700

    favicon

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <Route exact path="/videos/:id" component={Videos} />
           <Route exact path="/list" component={ProjectList} />
           <Route exact path="/home" component={Landing} />
-          <Route exact path="/" component={Landing} />
+          <Route path="/" component={Landing} />
         </BrowserRouter>
       </div>
     );
